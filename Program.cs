34582485830db831adacdf9fabdf54034abb0c97
HamBellMB35 <hbrskates@gmail.com>
2024-05-31 03:44:07 -0300

    Changed the default HTTP/HTTPS redirection to be dependent on development mode

--- Program.cs
+++ Program.cs
@@ -8,13 +8,20 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// If we are in development we want swaggerUI
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// No SSL certificate on development so we set it up this way to avoid annoyances and warnings
+// We dont want https redirection if we are in development
+else
+{
+    app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
+}
+
 
 var summaries = new[]
 {
