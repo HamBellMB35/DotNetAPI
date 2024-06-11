33fc689811d40e8ff29dc0909019e665796bc3d1
HamBellMB35 <hbrskates@gmail.com>
2024-06-11 04:08:19 -0300

    Wrote a connection string to get conected to databse, created endpoint to test conenction

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,13 +8,19 @@ namespace DotnetAPI.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-
-    public UserController()
+    DataContextDapper _dapper;
+    public UserController(IConfiguration config)
     {
-
+        //Console.WriteLine(config.GetConnectionString("DefaultConnection"));
+        _dapper = new DataContextDapper(config);
     }
 
-    
+    [HttpGet("testConnection")]
+
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
 
     [HttpGet("GetUsers/{testValue}")]
     //public IActionResult Test()
