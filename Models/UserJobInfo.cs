07210ea57860752ac3a69d66de6143f19a5b2d84
HamBellMB35 <hbrskates@gmail.com>
2024-06-13 22:26:19 -0300

    Create Users, UserSlary, and UseJobInfo Models Deleted WeatherForecastController

---
+++ Models/UserJobInfo.cs
@@ -0,0 +1,28 @@
+namespace DotnetAPI
+{
+
+    public partial class UserJobInfo
+    {
+        public int UserId {get; set;}
+        public string JobTitle {get; set;} = "";
+        public string Department {get; set;} = "";
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Models/UserSalary.cs
@@ -0,0 +1,27 @@
+namespace DotnetAPI
+{
+
+    public partial class UserSalary
+    {
+        public int UserId {get; set;}
+        public decimal Salary {get; set;} 
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
