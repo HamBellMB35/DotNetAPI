48347414e63c803877a259cbc4237cde82779b02
HamBellMB35 <hbrskates@gmail.com>
2024-06-14 15:48:00 -0300

    Added first iterations for the methods EditUser and AddUser methods

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07210ea57860752ac3a69d66de6143f19a5b2d84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0fb07f7a8c901f48dcbfea54597471b619a8688")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
